3ee90e7be324a91f6275fcaa8de6db30762563eb
Michael Hicks <hicks.michael04@gmail.com>
2022-08-31 15:56:41 -0600

    fix: ok array spacing

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -35,7 +35,7 @@ class ProductTest < ActiveSupport::TestCase
   end
 
   test 'image url' do 
-    ok = %w( fred.gif fred.jpg fred.png FRED.JPG FRED. Jpg http://a.b.c/x/y/z/fred.gif)
+    ok = %w( fred.gif fred.jpg fred.png FRED.JPG FRED.Jpg http://a.b.c/x/y/z/fred.gif)
     bad = %w(fred.doc fred.gif/more fred.gif.more)
 
     ok.each do |image_url|
