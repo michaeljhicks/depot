84a17885e88fdb6a11ba29acfae78e11e1e4b60f
Michael Hicks <hicks.michael04@gmail.com>
2022-08-31 14:50:48 -0600

    add model validations

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,7 @@
 require "test_helper"
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+test 'product attributes must not be empty' do 
+  product = Product.new
+end 
 end
