d2d01b9eb659f2a2300c87f6fa54b3b5ff3b4dc7
Michael Hicks <hicks.michael04@gmail.com>
2022-09-12 11:20:43 -0600

    test: create @title instance variable

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -3,6 +3,7 @@ require "test_helper"
 class ProductsControllerTest < ActionDispatch::IntegrationTest
   setup do
     @product = products(:one)
+    @title = "The Great Book #{rand}"
   end
 
   test "should get index" do
@@ -15,13 +16,13 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
-  # test "should create product" do
-  #   assert_difference("Product.count") do
-  #     post products_url, params: { product: { description: @product.description, image_url: @product.image_url, price: @product.price, title: @product.title } }
-  #   end
+  test "should create product" do
+    assert_difference("Product.count") do
+      post products_url, params: { product: { description: @product.description, image_url: @product.image_url, price: @product.price, title: @title } }
+    end
 
-  #   assert_redirected_to product_url(Product.last)
-  # end
+    assert_redirected_to product_url(Product.last)
+  end
 
   test "should show product" do
     get product_url(@product)
@@ -34,7 +35,7 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should update product" do
-    patch product_url(@product), params: { product: { description: @product.description, image_url: @product.image_url, price: @product.price, title: @product.title } }
+    patch product_url(@product), params: { product: { description: @product.description, image_url: @product.image_url, price: @product.price, title: @title } }
     assert_redirected_to product_url(@product)
   end
 
